16ccf1e6950e7ffe10060bb5c34973d3048ab88c
Stephen Connolly <stephen.alan.connolly@gmail.com>
2013-02-18 10:37:40 +0000

    generify the scripts

--- update-check.sh
+++ update-check.sh
@@ -1,30 +1,13 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-parse-pom() {
-    if [ ! -f "$1" ] ; then
-        echo "$1 is not a file" 1>&2
-        return 1
-    fi
-    xpath "$1" '/project/version/text()' 2>/dev/null | sed -e 's/-SNAPSHOT//' 
-    if [ ${PIPESTATUS[0]} -ne 0 ] ; then
-        return 1
-    fi
-}
-
-parse-json() {
-    if [ ! -f "$1" ] ; then
-        echo "$1 is not a file" 1>&2
-        return 1
-    fi
-
-    cat "$1" | python -c 'import json,sys;obj=json.load(sys.stdin);print obj["version"]' 
-}
+source "$DIR/redist-tools"
+source "$DIR/redist-config"
 
 if [ ! -d "$DIR/target/upstream" ] ; then
     rm -rf "$DIR/target/upstream"
     mkdir -p "$DIR/target/upstream"
-    git clone git://github.com/segmentio/analytics.js.git "$DIR/target/upstream"
+    git clone $UPSTREAM_GIT_URL "$DIR/target/upstream"
 else
 	cd "$DIR/target/upstream"
 	git fetch origin
@@ -37,7 +20,7 @@ fi
 
 if [ $? -eq 1 ] ; then
     echo "Searching for update..."
-    GOOD_REV=$(xpath "$DIR/pom.xml" '/project/properties/analytics-branch/text()')
+    GOOD_REV=$(xpath "$DIR/pom.xml" "/project/properties/$POM_PROPERTY/text()")
     if [ $? -ne 0 ] ; then
     	echo "Cannot parse pom.xml, check the scripts are still valid"
     	exit 255
@@ -54,7 +37,7 @@ if [ $? -eq 1 ] ; then
     cd -
     EXACT=$(xpath "$DIR/pom.xml" '/project/version/text()' 2>/dev/null)
     REPL=$(parse-json "$DIR/target/upstream/package.json")
-    sed -e "s:<version>$EXACT</version>:<version>$REPL-SNAPSHOT</version>:;s:<analytics-branch>$GOOD_REV</analytics-branch>:<analytics-branch>$BAD_REV</analytics-branch>:" < "$DIR/pom.xml" > "$DIR/pom.xml.new"
+    sed -e "s:<version>$EXACT</version>:<version>$REPL-SNAPSHOT</version>:;s:<$POM_PROPERTY>$GOOD_REV</$POM_PROPERTY>:<$POM_PROPERTY>$BAD_REV</$POM_PROPERTY>:" < "$DIR/pom.xml" > "$DIR/pom.xml.new"
     mvn clean verify -f "$DIR/pom.xml.new"
     if [ $? -eq 0 ] ; then
     	mv -f "$DIR/pom.xml.new" "$DIR/pom.xml"
